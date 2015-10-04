e80ab4b1bffa113c226f4d9fe0a2f2fdff741692
Yurii Sydoriak <yura.sid@gmail.com>
2015-10-04 23:44:44 +0300

    Making more detailed logging for Login class. Fixing a bug when users cannt log in (i forget pepper= param in function security.check_password_hash() and generate perrer every time new for security.password_hash. Idiot.

--- db_models/user_model.py
+++ db_models/user_model.py
@@ -3,6 +3,10 @@ __author__ = 'yura'
 from google.appengine.ext import ndb
 from webapp2_extras import security
 from lib.cookie import make_token_cookie
+import logging
+
+
+PEPPER = '28542283eb566945894f56eb22411fb291b1f845$sha1$saEPmYNMPcTAyevIivSQiD'
 
 
 class Login(ndb.Model):
@@ -13,34 +17,52 @@ class Login(ndb.Model):
 
     @classmethod
     def create_user(cls, username=None, raw_password=None, email=None):
+        logging.info('LoginClass:create_user() => Start creating user')
+
         if not(username and raw_password):
+            logging.error('LoginClass:create_user() => username or raw_password is empty, return None')
             return None
+
         salt = security.generate_random_string(length=30)
-        pw_hash = security.generate_password_hash(raw_password, pepper=salt)
+        logging.info('LoginClass:create_user() => Making salt for password hash ... < %s >' % salt)
+
+        pw_hash = security.generate_password_hash(raw_password, pepper=PEPPER)
+        logging.info('LoginClass:create_user() => Generatin password hash ... < %s >' % pw_hash)
         u = Login()
         u.populate(username=username,
                    pw_hash=pw_hash,
                    email=email)
+        logging.info('LoginClass:create_user() => Make and return Login instance')
         return u
 
     @classmethod
-    def check_user_password(cls, login=None, raw_password=None, email=None):
+    def check_user_password(cls, login=None, raw_password=None):
         """
         :param login:
             login
         :param raw_password:
             password
-        :param email:
-            email
-        :return:
-            True or False
+       :return:
+            Login instance or None
         """
+        logging.info('LoginClass:check_user_password() => Strat check_user_password()')
         if not raw_password:
-            return False
-        u = cls.query(Login.username == login).get() if login else cls.query(Login.email == email).get()
-        if not u:
+            logging.info('LoginClass:check_user_password() => raw_password is empty, return None')
             return None
-        return u if security.check_password_hash(raw_password, u.pw_hash) else None
+        logging.info('LoginClass:check_user_password() => Making QUERY to DB to get user instance')
+        u = cls.query(Login.username == login).get()
+        if not u:
+            logging.error('LoginClass:check_user_password() => User NOT found. Return None')
+            return None
+        logging.info('LoginClass:check_user_password() => User FOUND')
+
+        # comment for production
+        # return u if security.check_password_hash(raw_password, u.pw_hash, pepper=PEPPER) else None
+
+        sec_check = security.check_password_hash(raw_password, u.pw_hash, pepper=PEPPER)
+        logging.info('LoginClass:check_user_password() => Result of security.check_password_hash() is %s' % sec_check)
+        logging.info('LoginClass:check_user_password() => Return user instance or if True and None otherwise')
+        return u if sec_check else None
 
     @classmethod
     def _query_by_username(cls, username):
