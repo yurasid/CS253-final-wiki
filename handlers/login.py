c6e3e7263981277ab09c8ac7807f9a1988dffef5
Yurii Sydoriak <yura.sid@gmail.com>
2015-10-05 09:32:39 +0300

    Add paar log messages to LoginHandler

--- handlers/login.py
+++ handlers/login.py
@@ -9,11 +9,23 @@ from db_models.user_model import Login
 class LoginHandler(MainHandler):
 
     def get(self):
+
+        logging.info('LoginHandler:get() => Start GET')
+
         u = self.track()
+
+        logging.info('LoginHandler:get() => Call track() method and getting user instance')
+
         if u:
+
+            logging.info('LoginHandler:get() => Valid cookie found. User that log in is ... ' % u.username)
+            logging.info('LoginHandler:get() => Redirecting to "/"(main page)')
+
             self.redirect('/')
         else:
+
             logging.info('LoginHandler:get() => No valid cookie, Login start ...')
+
             self.render('login.jinja2')
 
     def post(self):
@@ -23,23 +35,39 @@ class LoginHandler(MainHandler):
         raw_password = self.request.get('password')
 
         if not (username and raw_password):
-            logging.error('LoginHandler:post() => User or password blank, render form whith error msg ...')
+
+            logging.error('LoginHandler:post() => User or password blank, render form with error msg ...')
+
             self.render('login.jinja2', error='Login or password fail')
             return
 
         logging.info('LoginHandler:post() => Login and password verify ...')
+        logging.info('LoginHandler:post() => Call Login.check_user_password() method ...')
+
         u = Login.check_user_password(login=username, raw_password=raw_password)
         if u:
+
             logging.info('LoginHandler:post() => Login success, user in DB. Set cookie')
+
             self.set_cookie(u.make_cookie())
+
+            logging.error('LoginHandler:post() => Redirecting to main page')
+
             self.redirect('/')
         else:
+
             logging.error('LoginHandler:post() => User or passwprd fail. No user in DB, or wrong password')
+
             self.delete_cookie()
+
+            logging.error('LoginHandler:post() => User or password FAIL, render form with error msg ...')
             self.render('login.jinja2', error='Login or password fail')
 
 
 class LogoutHandler(MainHandler):
     def get(self):
+        logging.error('LogoutHandler:get() => Start GET')
         self.delete_cookie()
+
+        logging.error('LogoutHandler:get() => Redirecting to main page')
         self.redirect('/')
